49af3fc9fd0e43572a4a9ba759d4ebd4e6b0e2bc
Will Aronow <waronow123@gmail.com>
2020-12-28 15:08:05 -0700

    solved exercise 20

--- javascript_basics/exercise-20_template-literals/index.js
+++ javascript_basics/exercise-20_template-literals/index.js
@@ -1,8 +1,8 @@
 // create a new variable called roll, that will be a string that
 // displays each die # as well as the sum
 
-// ex - die 1 = 3
-//      die 2 = 5
+// ex - die1 = 3
+//      die2 = 5
 //      roll = "You rolled a 3 and a 5. They sum to 8"
 
 // NO TOUCHING! (please)
@@ -10,3 +10,5 @@ const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
 const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
 
 // YOUR CODE BELOW THIS LINE:
+
+const roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}`;
