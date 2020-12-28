a27f46c834a8710adca9d4bc85704b0c89e0a54e
Will Aronow <waronow123@gmail.com>
2020-12-28 14:28:18 -0700

    created exercise 18

---
+++ javascript_basics/exercise-18_string-methods/index.js
@@ -0,0 +1,8 @@
+// create variable called whisper that is a lowercased version of
+// the variable message, with all the extra space removed
+// DO NOT ALTER THE ORIGINAL variable
+
+// DON'T TOUCH THIS LINE!
+const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+
+// YOUR CODE BELOW THIS LINE:
