aede4a7f2805b965334499cbe6d1ed5772acacb0
Will Aronow <waronow123@gmail.com>
2020-12-28 14:41:17 -0700

    created exercise 19

---
+++ javascript_basics/exercise-19_more-string-methods/index.js
@@ -0,0 +1,8 @@
+// using string methods create the string beard and store it in a new
+// variable called facialHair
+// DO NOT MODIFY THE ORIGINAL STRING VARIABLE
+
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
