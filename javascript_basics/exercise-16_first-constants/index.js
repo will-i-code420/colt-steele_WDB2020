862557346af52f7185829cfc3bdc0d844602d7fe
Will Aronow <waronow123@gmail.com>
2020-12-28 11:57:22 -0700

    solved exercise

--- javascript_basics/exercise-16_first-constants/index.js
+++ javascript_basics/exercise-16_first-constants/index.js
@@ -1,2 +1,5 @@
 // define constant variable called boilingPointC and set to 100
 // define constant variable called boilingPointF and set to 212
+
+const boilingPointC = 100;
+const boilingPointF = 212;
