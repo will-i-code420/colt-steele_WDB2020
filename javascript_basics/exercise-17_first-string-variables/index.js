7577f1b6aea4ec952f1d0363f36ec16f1af985d2
Will Aronow <waronow123@gmail.com>
2020-12-28 14:08:18 -0700

    solved exercise 17

--- javascript_basics/exercise-17_first-string-variables/index.js
+++ javascript_basics/exercise-17_first-string-variables/index.js
@@ -1,2 +1,5 @@
 // define string variable bestColor with string purple
 // define string variable quote with string You had me at "hello"
+
+let bestColor = 'purple';
+const quote = 'You had me at "hello"';
