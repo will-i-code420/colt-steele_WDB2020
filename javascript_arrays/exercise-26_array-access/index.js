a10f10aacf885f50ffc0561c366559e73eb5f4a2
Will Aronow <waronow123@gmail.com>
2020-12-30 12:04:35 -0700

    created exercise 26

---
+++ javascript_arrays/exercise-26_array-access/index.js
@@ -0,0 +1,7 @@
+// make the following changes to leaderboard array without touching original array
+// 1) change 2nd name from Lua to Luna
+// 2) Bellatrix has been replaced by draco
+
+const leaderboard = ['Harry', 'Lua', 'Hermoine', 'Bellatrix']; //DON'T TOUCH THIS LINE!
+
+// YOUR CODE BELOW HERE:
