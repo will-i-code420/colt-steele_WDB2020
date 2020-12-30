40bf4cb48a2a4bc5d086555b042a35413a3c8d01
Will Aronow <waronow123@gmail.com>
2020-12-30 15:43:51 -0700

    created exercise 29

---
+++ javascript_object-literals/exercise-29_first-object/index.js
@@ -0,0 +1,5 @@
+// create a new variable called product, it will be an object literal w/ the following properties
+// 1) name set to Gummy Bears
+// 2) inStock set to true
+// 3) price set to 1.99
+// 4) flavors set to an array of at least 3 strings
