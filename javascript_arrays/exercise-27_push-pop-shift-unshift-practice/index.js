93a2473027060aa2673367a95214ed1dfb8a353c
Will Aronow <waronow123@gmail.com>
2020-12-30 12:19:44 -0700

    created exercise 27

---
+++ javascript_arrays/exercise-27_push-pop-shift-unshift-practice/index.js
@@ -0,0 +1,8 @@
+// fix the planets array by doing the following
+// 1) remove the 1st element, The Moon
+// 2) add Saturn to the end of the array
+// 3) add Mercury as the 1st element
+
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
