dc1d1634dbf738c076d7c42a2d829b683daf0e7c
Will Aronow <waronow123@gmail.com>
2020-12-30 12:05:34 -0700

    solved exercise 26

--- javascript_arrays/exercise-26_array-access/index.js
+++ javascript_arrays/exercise-26_array-access/index.js
@@ -1,7 +1,9 @@
 // make the following changes to leaderboard array without touching original array
 // 1) change 2nd name from Lua to Luna
-// 2) Bellatrix has been replaced by draco
+// 2) Bellatrix has been replaced by Draco
 
 const leaderboard = ['Harry', 'Lua', 'Hermoine', 'Bellatrix']; //DON'T TOUCH THIS LINE!
 
 // YOUR CODE BELOW HERE:
+leaderboard[1] = 'Luna';
+leaderboard[3] = 'Draco';
