ba8ed81adf362a47979e4b8bc0190c20a3e017d8
Will Aronow <waronow123@gmail.com>
2020-12-30 12:56:15 -0700

    created exercise 28

---
+++ javascript_arrays/exercise-28_nested-arrays/index.js
@@ -0,0 +1,12 @@
+// add Hugo to the null spot in the airplaneSeats array
+
+// DO NOT TOUCH!!! (please)
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+// YOUR CODE GOES BELOW THIS LINE:
