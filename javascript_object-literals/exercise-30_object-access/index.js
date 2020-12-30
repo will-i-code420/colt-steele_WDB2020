9349505ca7623a8883a53b1cb45b9937d92f04e2
Will Aronow <waronow123@gmail.com>
2020-12-30 15:55:05 -0700

    created exercise 30

---
+++ javascript_object-literals/exercise-30_object-access/index.js
@@ -0,0 +1,15 @@
+// create a variable named fullAddress that points to the string info
+// in the restaurant variable provided
+// fullAddress should use the address, city, state, and zipcode
+// example "64 Johnson Ave, Brooklyn, NY 11206"
+
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
