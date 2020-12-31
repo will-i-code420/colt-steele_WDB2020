2e1bd49d395ffabed53c015c869e883bd6f2c8e5
Will Aronow <waronow123@gmail.com>
2020-12-30 21:04:08 -0700

    created exercise 33

---
+++ javascript_loops/exercise-33_iterating-arrays/index.js
@@ -0,0 +1,5 @@
+// loop of the variable people and print out each name in UPPERCASE
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
