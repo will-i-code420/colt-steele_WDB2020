a77a9e360410c8fd2ed544d6840379b7c8b3bde2
Will Aronow <waronow123@gmail.com>
2021-01-06 22:41:13 -0700

    updated instructions after watching more of the vid

--- javascript_loops/guessing_game/index.js
+++ javascript_loops/guessing_game/index.js
@@ -1,5 +1,8 @@
-// create an app that prompts user to guess a number between 1-20
-// once the number is guessed console the number of guess' used
+// create an app that prompts the user enter a max number
+// make sure the user enters a number
+// create a random number between 1 and the max number entered
+// make the user guess the number while tracking the number of guess'
+
 
 const num = Math.floor(Math.random() * 20) + 1;
 let guess = prompt('Guess a number between 1 and 20');
