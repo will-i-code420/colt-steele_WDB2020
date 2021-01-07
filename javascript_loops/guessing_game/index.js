2ec50dc8dae5dfa5d2a127946e7cb386ed2473ef
Will Aronow <waronow123@gmail.com>
2021-01-06 22:47:11 -0700

    re-wrote solution based on new instructions

--- javascript_loops/guessing_game/index.js
+++ javascript_loops/guessing_game/index.js
@@ -1,13 +1,18 @@
 // create an app that prompts the user enter a max number
 // make sure the user enters a number
 // create a random number between 1 and the max number entered
-// make the user guess the number while tracking the number of guess'
+// make the user guess the number while tracking number of guess'
+// when user guess' correctly console the number of guess'
 
 
-const num = Math.floor(Math.random() * 20) + 1;
-let guess = prompt('Guess a number between 1 and 20');
+let max = parseInt(prompt('Input Maximum Number'));
+if (max === NaN) {
+  max = parseInt(prompt('Input Maximum Number'));
+}
+const num = Math.floor(Math.random() * max) + 1;
 let numOfGuess = 0;
-while (guess !== num) {
+let guess = parseInt(prompt('Guess a number between 1 and 20'));
+while (guess !== num && guess === NaN) {
   guess = prompt('Guess a number between 1 and 20');
   numOfGuess++;
 }
