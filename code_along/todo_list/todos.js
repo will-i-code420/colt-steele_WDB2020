47d20e9422631730d88edc74a585c043a5a7a5fc
Will Aronow <waronow123@gmail.com>
2021-01-07 22:45:18 -0700

    changed new command prompts in loop due to else statement making 2nd input necessary

--- code_along/todo_list/todos.js
+++ code_along/todo_list/todos.js
@@ -5,6 +5,7 @@ while (command !== 'quit' && command !== 'q') {
     let todo = prompt('Input New TODO Item:');
     todos.push(todo);
     console.log(`${todo} added to the list`);
+    command = prompt('Input A Command').toLowerCase();
   } else if (command === 'list') {
     const length = todos.length;
     console.log('******');
@@ -12,6 +13,7 @@ while (command !== 'quit' && command !== 'q') {
       console.log(`${i + 1}:  ${todos[i]}`);
     }
     console.log('******');
+    command = prompt('Input A Command').toLowerCase();
   } else if (command === 'delete') {
     let length = todos.length;
     let index = parseInt(prompt('Which number to delete?')) - 1;
@@ -26,9 +28,9 @@ while (command !== 'quit' && command !== 'q') {
       console.log(`${i + 1}:  ${todos[i]}`);
     }
     console.log('******');
+    command = prompt('Input A Command').toLowerCase();
   } else {
     command = prompt('Unknown Command, Input A VALID Command!!').toLowerCase();
   }
-  command = prompt('Input A Command').toLowerCase();
 }
 console.log('YOU HAVE QUIT THE APP');
