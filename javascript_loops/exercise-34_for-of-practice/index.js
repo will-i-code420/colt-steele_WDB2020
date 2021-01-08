6945be3a8eace07d533b7dffa958fefa6e9ae503
Will Aronow <waronow123@gmail.com>
2021-01-07 20:14:56 -0700

    created exercise 34

---
+++ javascript_loops/exercise-34_for-of-practice/index.js
@@ -0,0 +1,5 @@
+// use a for...of loop to print out the square value of each number in the array numbers
+
+const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
