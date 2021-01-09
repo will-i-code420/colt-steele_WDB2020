22999105cb8ca1a2953937e2c701ab68ae7e9bcb
Will Aronow <waronow123@gmail.com>
2021-01-08 20:47:23 -0700

    solved exercise 36

--- javascript_Functions_Intro/exercise-36_rant/index.js
+++ javascript_Functions_Intro/exercise-36_rant/index.js
@@ -1,2 +1,8 @@
 // Define a function called rant that accepts a string argument called message
 // ran should the print out the argument 3 times
+
+function rant(message) {
+  console.log(`${message.toUpperCase()}`);
+  console.log(`${message.toUpperCase()}`);
+  console.log(`${message.toUpperCase()}`);
+}
