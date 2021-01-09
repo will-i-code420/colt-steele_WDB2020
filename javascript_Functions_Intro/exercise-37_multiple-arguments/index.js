0f4a850b04bfe99f271cd7cec4e7f4c9f2d876da
Will Aronow <waronow123@gmail.com>
2021-01-08 21:01:21 -0700

    solved exervise 37

--- javascript_Functions_Intro/exercise-37_multiple-arguments/index.js
+++ javascript_Functions_Intro/exercise-37_multiple-arguments/index.js
@@ -1,2 +1,10 @@
-// write a function called isSnakeEyes which accepts two inputs
+// write a function called isSnakeEyes which accepts two inputs representing dice
 // if both numbers are 1 print Snake Eyes! otherwise print Not Snake Eyes!
+
+function isSnakeEyes(die1, die2) {
+  if (die1 === 1 && die2 === 1) {
+    console.log('Snake Eyes!');
+  } else {
+    console.log('Not Snake Eyes!');
+  }
+}
