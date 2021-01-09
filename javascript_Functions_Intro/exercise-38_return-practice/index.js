3eed8dea535b09bedaa988396dc7debc97171d0a
Will Aronow <waronow123@gmail.com>
2021-01-08 21:10:26 -0700

    solved exercise 38

--- javascript_Functions_Intro/exercise-38_return-practice/index.js
+++ javascript_Functions_Intro/exercise-38_return-practice/index.js
@@ -1,2 +1,9 @@
 // write a function called multiply that accepts 2 number arguments
 // and returns their product
+
+function multiply(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return false;
+  }
+  return a * b;
+}
