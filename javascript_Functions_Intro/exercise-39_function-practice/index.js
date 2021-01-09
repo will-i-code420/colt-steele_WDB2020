8ed597dc7178da896ddfb8d514323b8362b75f10
Will Aronow <waronow123@gmail.com>
2021-01-08 21:14:43 -0700

    solved exercise 39

--- javascript_Functions_Intro/exercise-39_function-practice/index.js
+++ javascript_Functions_Intro/exercise-39_function-practice/index.js
@@ -1,2 +1,10 @@
 // write a function called isShortsWeather that will accept a single
 // number argument.  If this argument is >= 75 return true otherwise false
+
+function isShortsWeather(temp) {
+  if (temp >= 75) {
+    return true;
+  } else {
+    return false;
+  }
+}
