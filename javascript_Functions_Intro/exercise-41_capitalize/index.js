65244e2a17001561c7195010fb43c8527925e219
Will Aronow <waronow123@gmail.com>
2021-01-10 12:05:55 -0700

    solved exercise 41

--- javascript_Functions_Intro/exercise-41_capitalize/index.js
+++ javascript_Functions_Intro/exercise-41_capitalize/index.js
@@ -1,3 +1,7 @@
 // create a function called capitalize that accepts a string argument and
 // returns a new string with the first letter capitalized but leaves
 // the rest of the string unchanged
+
+function capitalize(str) {
+  return str.replace(str[0], str[0].toUpperCase());
+}
