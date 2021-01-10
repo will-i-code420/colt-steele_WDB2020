e57e5c9b7a0a214059937fae62c1b062714ef6ff
Will Aronow <waronow123@gmail.com>
2021-01-10 11:18:38 -0700

    created exercise 40

---
+++ javascript_Functions_Intro/exercise-40_last-element/index.js
@@ -0,0 +1,3 @@
+// write a function called lastElement which accepts a single array
+// argurment.  The function should return the last element of the array
+// without removing the element.  If array is empty return null
