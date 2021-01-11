89fd06eed0d195f13161ade563c4b6da5f4a6582
Will Aronow <waronow123@gmail.com>
2021-01-10 21:36:05 -0700

    solved exercise 42

--- javascript_Functions_Intro/exercise-43_days-of-the-week/index.js
+++ javascript_Functions_Intro/exercise-43_days-of-the-week/index.js
@@ -1,3 +1,9 @@
 // write a function called returnDay.  it will take 1 parameter (num between 1-7)
 // and returns the day of the week (1 being monday, 2 tuesday, etc..)
 // if num < 1 or > 7 it should return null
+
+function returnDay(num) {
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  if (num < 1 || num > 7) return null;
+  return days[num - 1];
+}
