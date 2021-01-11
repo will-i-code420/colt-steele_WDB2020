89297268fe17793b3b0893f1e09413cc0fe381c2
Will Aronow <waronow123@gmail.com>
2021-01-10 21:21:40 -0700

    solved exercise 42

--- javascript_Functions_Intro/exercise-42_sum-array/index.js
+++ javascript_Functions_Intro/exercise-42_sum-array/index.js
@@ -1,2 +1,10 @@
 // write a function called sumArray which accepts 1 argument, an array of numbers
 // it should return the sum of the array
+
+function sumArray(nums) {
+  let total = 0;
+  for (let i = 0; i < nums.length; i++) {
+    total += nums[i];
+  }
+  return total;
+}
