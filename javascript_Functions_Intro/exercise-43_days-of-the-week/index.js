676d077cd3553614d4ef71c412794214d051870f
Will Aronow <waronow123@gmail.com>
2021-01-10 21:24:02 -0700

    created exercise 43

---
+++ javascript_Functions_Intro/exercise-43_days-of-the-week/index.js
@@ -0,0 +1,3 @@
+// write a function called returnDay.  it will take 1 parameter (num between 1-7)
+// and returns the day of the week (1 being monday, 2 tuesday, etc..)
+// if num < 1 or > 7 it should return null
