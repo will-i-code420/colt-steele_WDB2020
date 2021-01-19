b3fdded1f114451c54c6ff0696a5427b72e3f134
Will Aronow <waronow123@gmail.com>
2021-01-19 16:50:30 -0700

    solved exercise 48

--- javascript_callbacks-array_methods/exercise-48_arrow-functions/index.js
+++ javascript_callbacks-array_methods/exercise-48_arrow-functions/index.js
@@ -1,2 +1,6 @@
 // write an arrow function expression called greet that accepts a string
-// argument representing a person's name.  it should return Hey, [name]!
+// argument representing a person's name.  it should return Hey [name]!
+
+const greet = name => {
+  return `Hey ${name}!`;
+}
