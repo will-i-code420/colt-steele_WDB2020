c690f4d88deed0c03164b2adc1f210b5f763e2fa
Will Aronow <waronow123@gmail.com>
2021-01-18 21:11:42 -0700

    created exercise 46

---
+++ javascript_more-functions/exercise-46_egg-laying/index.js
@@ -0,0 +1,5 @@
+// define an object called hen with the following 3 properties
+// 1) name set to Helen
+// 2) eggCount set to 0
+// 3) layAnEgg is a method which increments eggCount by 1 & returns the string EGG
+// be sure to use this
