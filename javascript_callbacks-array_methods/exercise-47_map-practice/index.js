73d4fdbdf5072be376f86f26744c080af821afda
Will Aronow <waronow123@gmail.com>
2021-01-19 13:16:32 -0700

    created exercise 47

---
+++ javascript_callbacks-array_methods/exercise-47_map-practice/index.js
@@ -0,0 +1,3 @@
+// define a function called cleanNames that accepts an array of strings
+// which may have additional space characters at the beginning and end
+// the function should use the map method to return a new array of trimmed names
