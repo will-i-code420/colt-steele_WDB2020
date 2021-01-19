f1817dbf5b3af98d72ad5235dc44813036526542
Will Aronow <waronow123@gmail.com>
2021-01-18 20:56:23 -0700

    created exercise 45

---
+++ javascript_more-functions/exercise-45_methods/index.js
@@ -0,0 +1,4 @@
+// define an object called square which holds methods to do with geometry
+// of squares.  It should contain 2 methods
+// 1) area - which should accept the length of a side and return it squared
+// 2) perimeter - which should accept the length of a side and return it multiplied by 4
