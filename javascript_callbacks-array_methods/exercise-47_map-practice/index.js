8a7c950a1a13d5f0454fd542c9f37c4f3959b71d
Will Aronow <waronow123@gmail.com>
2021-01-19 13:19:49 -0700

    shortned solution

--- javascript_callbacks-array_methods/exercise-47_map-practice/index.js
+++ javascript_callbacks-array_methods/exercise-47_map-practice/index.js
@@ -3,8 +3,7 @@
 // the function should use the map method to return a new array of trimmed names
 
 function cleanNames(names) {
-  let trimmed = names.map(function (name) {
+  return names.map(function (name) {
     return name.trim();
   })
-  return trimmed;
 }
