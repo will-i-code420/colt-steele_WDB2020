68ac966e6cb065d6b2f45051dc56e7f4b79cb832
Will Aronow <waronow123@gmail.com>
2021-01-19 13:18:58 -0700

    solved exercise 47

--- javascript_callbacks-array_methods/exercise-47_map-practice/index.js
+++ javascript_callbacks-array_methods/exercise-47_map-practice/index.js
@@ -1,3 +1,10 @@
 // define a function called cleanNames that accepts an array of strings
 // which may have additional space characters at the beginning and end
 // the function should use the map method to return a new array of trimmed names
+
+function cleanNames(names) {
+  let trimmed = names.map(function (name) {
+    return name.trim();
+  })
+  return trimmed;
+}
