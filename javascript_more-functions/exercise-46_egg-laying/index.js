8534ac61f2b318ade0761914d5e56ce607f085ca
Will Aronow <waronow123@gmail.com>
2021-01-18 21:29:28 -0700

    solved exercise 46

--- javascript_more-functions/exercise-46_egg-laying/index.js
+++ javascript_more-functions/exercise-46_egg-laying/index.js
@@ -3,3 +3,12 @@
 // 2) eggCount set to 0
 // 3) layAnEgg is a method which increments eggCount by 1 & returns the string EGG
 // be sure to use this
+
+const hen = {
+  name: 'Helen',
+  eggCount: 0,
+  layAnEgg() {
+    this.eggCount++;
+    return 'EGG';
+  }
+}
