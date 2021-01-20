46066d35a0afe40e25dab306bdf5490f75542539
Will Aronow <waronow123@gmail.com>
2021-01-19 20:16:58 -0700

    solved exercise 50

--- javascript_callbacks-array_methods/exercise-50_some-every/index.js
+++ javascript_callbacks-array_methods/exercise-50_some-every/index.js
@@ -1,1 +1,6 @@
-// 
+// define a function called allEvens that accepts a single array of numbers
+// if the array is all even return true otherwise false
+
+const allEvens = numbers => {
+  return numbers.every(num => num % 2 === 0);
+}
