ae66c6670d52ae69c4baba4057c1a41212479eb9
Will Aronow <waronow123@gmail.com>
2021-01-19 18:33:03 -0700

    solved exercise 49

--- javascript_callbacks-array_methods/exercise-49_filter/index.js
+++ javascript_callbacks-array_methods/exercise-49_filter/index.js
@@ -1,2 +1,6 @@
 // write a function called validUserNames that accepts an array of validUserNames
 // it should return a new array containing usernames less than 10 characters long
+
+const validUserNames = names => {
+  return names.filter(name => name.length < 10);
+}
