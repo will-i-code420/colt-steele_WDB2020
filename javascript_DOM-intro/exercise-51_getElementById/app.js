8f62225eab929ddf67627e279dc2f49475cc7187
Will Aronow <waronow123@gmail.com>
2021-01-21 20:14:28 -0700

    solved exercise 51

--- javascript_DOM-intro/exercise-51_getElementById/app.js
+++ javascript_DOM-intro/exercise-51_getElementById/app.js
@@ -1,3 +1,6 @@
 // write code the does the following using getElementById
 // 1) save the image element to a variable called image
 // 2) save the h1 element to a variable called heading
+
+const image = document.getElementById('unicorn');
+const heading = document.getElementById('mainheading');
