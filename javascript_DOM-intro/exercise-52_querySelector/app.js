948d29ec5900b9e2b5fc18d0cfeb92d093ea2c6b
Will Aronow <waronow123@gmail.com>
2021-01-21 21:05:28 -0700

    added instructiosn to js file

---
+++ javascript_DOM-intro/exercise-52_querySelector/app.js
@@ -0,0 +1,4 @@
+// use querySelector and querySelectorAll to do the following
+// 1) select all elements with class of done into a variable doneTodos
+// 2) select one checkbox and save it to a variable called checkbox
+//    use the type attribute
