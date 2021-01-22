7363fcbc962e1d4d1e9bca62094c81c2c3d2b3ca
Will Aronow <waronow123@gmail.com>
2021-01-21 22:47:16 -0700

    solved exercise 54

--- javascript_DOM-intro/exercise-54_manipulating-attributes/app.js
+++ javascript_DOM-intro/exercise-54_manipulating-attributes/app.js
@@ -1,3 +1,6 @@
 // select the image element and
 // 1) change its source to https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg
-// 2) change the alt text to be 
+// 2) change the alt text to be chicken
+
+document.querySelector('img').src = 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg';
+document.querySelector('img').alt = 'chicken';
