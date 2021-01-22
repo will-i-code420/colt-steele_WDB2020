8eca135a3b00aa4232778e551a6d7b398fd6faad
Will Aronow <waronow123@gmail.com>
2021-01-21 21:09:08 -0700

    solved exercise 52

--- javascript_DOM-intro/exercise-52_querySelector/app.js
+++ javascript_DOM-intro/exercise-52_querySelector/app.js
@@ -2,3 +2,6 @@
 // 1) select all elements with class of done into a variable doneTodos
 // 2) select one checkbox and save it to a variable called checkbox
 //    use the type attribute
+
+const doneTodos = document.querySelectorAll('.done');
+const checkbox = document.querySelector('input[type="checkbox"]');
