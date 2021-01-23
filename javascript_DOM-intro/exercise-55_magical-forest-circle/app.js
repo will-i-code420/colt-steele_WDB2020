2eb07add1c963db30a740f4faef6c4212b568bec
Will Aronow <waronow123@gmail.com>
2021-01-22 21:25:23 -0700

    solved exercise 55

--- javascript_DOM-intro/exercise-55_magical-forest-circle/app.js
+++ javascript_DOM-intro/exercise-55_magical-forest-circle/app.js
@@ -1,2 +1,6 @@
 // 1) select the element with the id container, set text-alignment to center
 // 2) select the image element and set width to 150px and border-radius to 50%
+
+document.querySelector('#container').style.textAlign = 'center';
+document.querySelector('img').style.width = '150px';
+document.querySelector('img').style.borderRadius = '50%';
