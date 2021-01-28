598c88b512c743a63731fd03c9914c372eb9202b
Will Aronow <waronow123@gmail.com>
2021-01-27 20:28:15 -0700

    solved exercise 56, renamed index.js to .html

--- javascript_DOM-intro/exercise-56_rainbow-text/app.js
+++ javascript_DOM-intro/exercise-56_rainbow-text/app.js
@@ -1,1 +1,19 @@
 // select all the spans and assign each of them one of the colors from the colors array
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const allSpans = document.querySelectorAll('span');
+
+/*
+works in chrome and firefox but not on course
+
+for (let i = 0; i < spans.length; i++) {
+  let currentColor = colors[i];
+  spans[i].style.color = `${currentColor}`;
+  counter++;
+}
+*/
+let counter = 0;
+for (let span of allSpans) {
+  span.style.color = `${colors[counter]}`;
+  counter++;
+}
