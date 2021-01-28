d78c5cd598e5d8923ce326171a61bd02e63e409d
Will Aronow <waronow123@gmail.com>
2021-01-27 20:41:18 -0700

    solved exercise 57

--- javascript_DOM-intro/exercise-57_classList/app.js
+++ javascript_DOM-intro/exercise-57_classList/app.js
@@ -1,1 +1,7 @@
 // use the classList property to invert which li elements have the highlight class
+
+const allItems = document.querySelectorAll('li');
+
+for (let item of allItems) {
+  item.classList.toggle('highlight');
+}
