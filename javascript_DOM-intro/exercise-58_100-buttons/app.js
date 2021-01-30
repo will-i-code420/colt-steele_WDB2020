6a657313eac9a8b9e36438930be41bd84fc33c6f
Will Aronow <waronow123@gmail.com>
2021-01-29 20:29:51 -0700

    solved exercise 58

--- javascript_DOM-intro/exercise-58_100-buttons/app.js
+++ javascript_DOM-intro/exercise-58_100-buttons/app.js
@@ -1,2 +1,10 @@
 // create 100 buttons inside element ID container
 // each button must have text inside it and you must use appendChild
+
+const element = document.querySelector('#container');
+
+for (let i = 0; i < 100; i++) {
+  let newBtn = document.createElement('button')
+  newBtn.innerText = `Button ${i + 1}`;
+  element.appendChild(newBtn);
+}
