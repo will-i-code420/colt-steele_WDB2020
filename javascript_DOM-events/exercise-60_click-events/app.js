9e99e5937823adf033d52d83c624375b58365499
Will Aronow <waronow123@gmail.com>
2021-01-30 20:42:35 -0700

    solved exercise 60

--- javascript_DOM-events/exercise-60_click-events/app.js
+++ javascript_DOM-events/exercise-60_click-events/app.js
@@ -1,3 +1,14 @@
 // add an event listener to each button provided
 // the hello button should log hello
 // the goodbye button should log goodbye
+
+const helloBtn = document.querySelector('#hello');
+const byeBtn = document.querySelector('#goodbye');
+
+helloBtn.addEventListener('click', function () {
+  console.log('hello');
+});
+
+byeBtn.addEventListener('click', function () {
+  console.log('goodbye');
+})
