4e44dee917b2d99ff11e034693c9d894a9132a4a
Will Aronow <waronow123@gmail.com>
2021-01-30 20:56:03 -0700

    created my solution prior to watching video after intro portion

--- javascript_DOM-events/RandomColors/Exercise/app.js
+++ javascript_DOM-events/RandomColors/Exercise/app.js
@@ -3,3 +3,18 @@
 
 const h1 = document.querySelector('h1');
 const btn = document.querySelector('button');
+
+function randomNumber() {
+  const num = Math.floor(Math.random() * 255) + 1;
+  return num;
+};
+
+btn.addEventListener('click', function() {
+  const body = document.querySelector('body')
+  const num1 = randomNumber();
+  const num2 = randomNumber();
+  const num3 = randomNumber();
+  const newBgColor = `rgb(${num1}, ${num2}, ${num3})`;
+  body.style.backgroundColor = `${newBgColor}`;
+  h1.innerText = `${newBgColor}`;
+});
