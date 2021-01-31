355461fbe67919451b5a93bab62252f50bb7544c
Will Aronow <waronow123@gmail.com>
2021-01-30 20:47:18 -0700

    created variables to hold the button and h1

--- javascript_DOM-events/RandomColors/Exercise/app.js
+++ javascript_DOM-events/RandomColors/Exercise/app.js
@@ -1,2 +1,5 @@
 // Make the background color of the body change to a random RGB Colors
 // make the h1 display the randomly generated RGB number
+
+const h1 = document.querySelector('h1');
+const btn = document.querySelector('button');
