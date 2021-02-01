4627847f0ce6149696df8e10047420d292ba7d20
Will Aronow <waronow123@gmail.com>
2021-02-01 15:51:18 -0700

    created exercise 61

---
+++ javascript_DOM-events/exercise-61_form-events/app.js
@@ -0,0 +1,7 @@
+// listen for the form submission and use prevent default
+// grab the values of the inputs
+// create a new li element and set the text to be the input values
+// append the new li to the ul and reset the inputs
+
+// Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
+const form = document.querySelector('form');
