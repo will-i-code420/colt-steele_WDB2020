5be55ecc8e7571f1501e8b23fce7dbb01a60076d
Will Aronow <waronow123@gmail.com>
2021-02-01 15:59:56 -0700

    solved exercise 61

--- javascript_DOM-events/exercise-61_form-events/app.js
+++ javascript_DOM-events/exercise-61_form-events/app.js
@@ -5,3 +5,14 @@
 
 // Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
 const form = document.querySelector('form');
+const groceryList = document.querySelector('#list');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const qty = form.elements.qty;
+  const product = form.elements.product;
+  const newLi = document.createElement('li');
+  newLi.innerText = `${qty.value} ${product.value}`;
+  groceryList.appendChild(newLi);
+  qty.value = '';
+  product.value = '';
+});
