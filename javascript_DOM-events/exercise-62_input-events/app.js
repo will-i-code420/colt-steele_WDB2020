8faee984c0baaf4cb652d933e38541218b124508
Will Aronow <waronow123@gmail.com>
2021-02-01 21:09:47 -0700

    solved exercise 62

--- javascript_DOM-events/exercise-62_input-events/app.js
+++ javascript_DOM-events/exercise-62_input-events/app.js
@@ -1,2 +1,12 @@
 // create an input event that updates the h1 to display
 // Welcome, current input & if the input is empty the h1 should read Enter Your Username
+
+const h1 = document.querySelector('h1');
+const input = document.querySelector('input');
+
+input.addEventListener('input', function(e) {
+  h1.textContent = `Welcome, ${input.value}`;
+  if (h1.textContent == 'Welcome, ') {
+    h1.textContent = 'Enter Your Username';
+  }
+});
