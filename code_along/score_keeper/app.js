064231fa8d4501a4447c10aa469ac344dc1ffc21
Will Aronow <waronow123@gmail.com>
2021-02-03 16:28:24 -0700

    refactored code to use boolean after watching end of video 1

--- code_along/score_keeper/app.js
+++ code_along/score_keeper/app.js
@@ -6,17 +6,26 @@ const p2ScoreContainer = document.querySelector('#p2-score');
 let p1Score = 0;
 let p2Score = 0;
 let winScore = 5;
+let isGameOver = false;
 
 p1Btn.addEventListener('click', e => {
-  p1Score++;
-  p1ScoreContainer.innerText = p1Score;
-  checkScore(p1Score);
+  if (!isGameOver) {
+    p1Score++;
+    if (p1Score === winScore) {
+      isGameOver = true;
+    }
+    p1ScoreContainer.innerText = p1Score;
+  }
 })
 
 p2Btn.addEventListener('click', e => {
-  p2Score++;
-  p2ScoreContainer.innerText = p2Score;
-  checkScore(p2Score);
+  if (!isGameOver) {
+    p2Score++;
+    if (p2Score === winScore) {
+      isGameOver = true;
+    }
+    p2ScoreContainer.innerText = p2Score;
+  }
 })
 
 resetBtn.addEventListener('click', e => {
@@ -24,13 +33,5 @@ resetBtn.addEventListener('click', e => {
   p2Score = 0;
   p1ScoreContainer.innerText = p1Score;
   p2ScoreContainer.innerText = p2Score;
-  p1Btn.disabled = false;
-  p2Btn.disabled = false;
+  isGameOver = false;
 })
-
-const checkScore = score => {
-  if (score === winScore) {
-    p1Btn.disabled = true;
-    p2Btn.disabled = true;
-  }
-}
