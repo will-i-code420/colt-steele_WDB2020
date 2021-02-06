dfa4517d2db367f148910091cc9c9dfb9b73ec15
Will Aronow <waronow123@gmail.com>
2021-02-05 20:40:32 -0700

    refactored code into single function using new objects

--- code_along/score_keeper/app.js
+++ code_along/score_keeper/app.js
@@ -15,49 +15,43 @@ let winScoreOption = document.querySelector('#win-score');
 let winScore = parseInt(winScoreOption.value);
 let isGameOver = false;
 
+function updateScores(player, opponent) {
+  if (!isGameOver) {
+    player.score++;
+    if (player.score === winScore) {
+      isGameOver = true;
+      player.btn.disabled = true;
+      opponent.btn.disabled = true;
+      player.display.classList.add('has-text-success');
+      opponent.display.classList.add('has-text-danger');
+    }
+    player.display.innerText = p1Score;
+  }
+};
+
 winScoreOption.addEventListener('change', e => {
   winScore = parseInt(e.target.value);
   resetGame();
 });
 
-p1Btn.addEventListener('click', e => {
-  if (!isGameOver) {
-    p1Score++;
-    if (p1Score === winScore) {
-      isGameOver = true;
-      p1Btn.disabled = true;
-      p2Btn.disabled = true;
-      p1ScoreContainer.classList.add('has-text-success');
-      p2ScoreContainer.classList.add('has-text-danger');
-    }
-    p1ScoreContainer.innerText = p1Score;
-  }
+p1.btn.addEventListener('click', e => {
+  updateScores(p1, p2);
 })
 
-p2Btn.addEventListener('click', e => {
-  if (!isGameOver) {
-    p2Score++;
-    if (p2Score === winScore) {
-      isGameOver = true;
-      p1Btn.disabled = true;
-      p2Btn.disabled = true;
-      p1ScoreContainer.classList.add('has-text-danger');
-      p2ScoreContainer.classList.add('has-text-success');
-    }
-    p2ScoreContainer.innerText = p2Score;
-  }
+p2.btn.addEventListener('click', e => {
+  updateScores(p2, p1);
 })
 
 resetBtn.addEventListener('click', resetGame);
 
 function resetGame() {
-  p1Score = 0;
-  p2Score = 0;
-  p1ScoreContainer.innerText = p1Score;
-  p2ScoreContainer.innerText = p2Score;
-  p1ScoreContainer.classList.remove('has-text-success', 'has-text-danger');
-  p2ScoreContainer.classList.remove('has-text-success', 'has-text-danger');
+  p1.score = 0;
+  p2.score = 0;
+  p1.display.innerText = p1.score;
+  p2.display.innerText = p2.score;
+  p1.display.classList.remove('has-text-success', 'has-text-danger');
+  p2.display.classList.remove('has-text-success', 'has-text-danger');
   isGameOver = false;
-  p1Btn.disabled = false;
-  p2Btn.disabled = false;
+  p1.btn.disabled = false;
+  p2.btn.disabled = false;
 }
