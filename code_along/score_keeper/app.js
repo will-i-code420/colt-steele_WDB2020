c8c13eb88f8f4798b41b95a7ae7b4182da422f28
Will Aronow <waronow123@gmail.com>
2021-02-07 23:02:25 -0700

    made it possible to add additional players and refactored game logic to work with more than 2 players

--- code_along/score_keeper/app.js
+++ code_along/score_keeper/app.js
@@ -2,43 +2,115 @@
 // animation for win
 // best 2 of 3 or more games for same players
 
-const player1 = {
-  score: 0,
-  btn: document.querySelector('#p1-btn'),
-  display: document.querySelector('#p1-score')
-};
-
-const player2 = {
-  score: 0,
-  btn: document.querySelector('#p2-btn'),
-  display: document.querySelector('#p2-score')
-};
-
-const resetBtn = document.querySelector('#reset-btn');
+/* Global Variables */
+let isGameOver = false;
+let resetBtn;
+const gameBtnsContainer = document.querySelector('#game-btns');
+const scoreDisplay = document.querySelector('#score-display');
 let winScoreOption = document.querySelector('#win-score');
 let winScore = parseInt(winScoreOption.value);
-let isGameOver = false;
+let numOfPlayersOption = document.querySelector('#player-select');
+let players = [];
+createPlayers(numOfPlayersOption.value);
 
-function updateScores(player, opponent) {
-  if (!isGameOver) {
-    player.score++;
-    if (player.score === winScore && winByTwo(player, opponent)) {
-      isGameOver = true;
-      player.btn.disabled = true;
-      opponent.btn.disabled = true;
-      player.display.classList.add('has-text-success');
-      opponent.display.classList.add('has-text-danger');
-    }
-    player.display.innerText = player.score;
+function createPlayers(numOfPlayers) {
+  // remove previous btns
+  while (gameBtnsContainer.firstChild) {
+    gameBtnsContainer.removeChild(gameBtnsContainer.firstChild);
   }
+
+  // remove previous score span tags
+  while (scoreDisplay.firstChild) {
+    scoreDisplay.removeChild(scoreDisplay.firstChild);
+  }
+
+  players = [];
+
+  for (let i = 0; i < numOfPlayers; i++) {
+    let newPlayer = {
+      score: 0,
+      btn: createBtn('player', i),
+      display: createScoreBoard(i, numOfPlayers)
+    };
+    players.push(newPlayer);
+  }
+  createBtn('reset');
 };
 
-function winByTwo(player, opponent) {
-  if (opponent.score + 1 === player.score) {
-    winScore++;
-    return false;
+function createScoreBoard(player, totalPlayers) {
+  let span = document.createElement('span');
+  span.id = `player${player + 1}-display`;
+  span.innerText = '0';
+  if (player === totalPlayers - 1) {
+    scoreDisplay.append(span);
+  } else {
+    scoreDisplay.append(span);
+    scoreDisplay.append(' to ');
   }
-  return true;
+  return document.querySelector(`#player${player + 1}-display`)
+};
+
+function createBtn(type, playerNum) {
+  const btnColors = ['is-primary', 'is-info', 'is-success', 'is-link'];
+  let newBtn = document.createElement('button');
+  if (type === 'reset') {
+    newBtn.id = `reset-btn`;
+    newBtn.onclick = resetGame;
+    newBtn.classList.add('button', 'is-large', 'is-danger', 'card-footer-item');
+    newBtn.innerText = 'Reset';
+    gameBtnsContainer.append(newBtn);
+    resetBtn = document.querySelector('#reset-btn');
+    return
+  }
+  newBtn.id = `player${playerNum + 1}-btn`;
+  newBtn.onclick = updateScores;
+  newBtn.classList.add('button', 'is-large', `${btnColors[playerNum]}`, 'card-footer-item');
+  newBtn.innerText = `Player ${playerNum + 1} +1`;
+  gameBtnsContainer.append(newBtn);
+  return document.querySelector(`#player${playerNum + 1}-btn`);
+};
+
+function updateScores(e) {
+  const regex = /[0-9]/g;
+  const playerId = e.target.id.match(regex);
+  let opponentIds = [];
+  let opponentScores = [];
+  for (let i = 0; i < players.length; i++) {
+    if (i + 1 != playerId) {
+      opponentIds.push(parseInt(players[i].btn.id.match(regex)) - 1);
+      opponentScores.push(players[i].score);
+    }
+  }
+  if (!isGameOver) {
+    players[playerId - 1].score++;
+    if (players[playerId - 1].score === winScore && winByTwo(players[playerId - 1].score, opponentScores)) {
+      isGameOver = true;
+      players[playerId - 1].btn.disabled = true;
+      players[playerId - 1].display.classList.add('has-text-success');
+      for (let opponent in opponentIds) {
+        players[opponentIds[opponent]].btn.disabled = true;
+        players[opponentIds[opponent]].display.classList.add('has-text-danger');
+      }
+    }
+  }
+  players[playerId - 1].display.innerText = players[playerId - 1].score;
+};
+
+function winByTwo(playerScore, opponentScores) {
+  let defeatList = [];
+  let wonByTwo;
+  for (let i = 0; i < opponentScores.length; i++) {
+    if (opponentScores[i] + 1 === playerScore) {
+      defeatList.push(false);
+    } else {
+      defeatList.push(true);
+    }
+  }
+  if (defeatList.includes(false)) {
+    winScore++;
+    return false
+  }
+  return true
 };
 
 winScoreOption.addEventListener('change', e => {
@@ -46,22 +118,20 @@ winScoreOption.addEventListener('change', e => {
   resetGame();
 });
 
-player1.btn.addEventListener('click', e => {
-  updateScores(player1, player2);
-})
-
-player2.btn.addEventListener('click', e => {
-  updateScores(player2, player1);
-})
+numOfPlayersOption.addEventListener('change', e => {
+  numOfPlayers = parseInt(e.target.value);
+  createPlayers(numOfPlayers);
+  resetGame();
+});
 
 resetBtn.addEventListener('click', resetGame);
 
 function resetGame() {
-  for (let p of [player1, player2]) {
-    p.score = 0;
-    p.display.innerText = p.score;
-    p.display.classList.remove('has-text-success', 'has-text-danger');
-    p.btn.disabled = false;
+  for (let player of [...players]) {
+    player.score = 0;
+    player.display.innerText = player.score;
+    player.display.classList.remove('has-text-success', 'has-text-danger');
+    player.btn.disabled = false;
   }
   isGameOver = false;
 }
