1271db6ac3053eabe08210be37745db5ac35e22a
Will Aronow <waronow123@gmail.com>
2021-02-14 22:38:00 -0700

    created basic card to display img, title and summary of shows and made logic work with it, used form submit event instead of watching button

--- javascript_AJAX-and-APIs/TVShowSearch/my-attempt/app.js
+++ javascript_AJAX-and-APIs/TVShowSearch/my-attempt/app.js
@@ -1,24 +1,34 @@
 // make use of images, title, score and some other things
 
-const formBtn = document.querySelector('#search-form-btn');
+const form = document.querySelector('#search-form');
 const searchResultsContainer = document.querySelector('#search-results-container');
-const searchResultsList = document.querySelector('#search-results-list');
 
 const findShow = async (e) => {
   e.preventDefault();
   // clear previous results
-  while (searchResultsList.firstChild) {
-    searchResultsList.removeChild(searchResultsList.firstChild);
+  while (searchResultsContainer.firstChild) {
+    searchResultsContainer.removeChild(searchResultsContainer.firstChild);
   }
-  let searchParam = document.querySelector('input[name="show"]').value;
+  let searchParam = form.elements.query.value;
   const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchParam}`);
   const shows = res.data;
   shows.forEach((show, i) => {
-    const newLi = document.createElement('li');
-    newLi.classList.add('search-result-item');
-    newLi.append(show.show.name)
-    searchResultsList.append(newLi);
+    const newCard = document.createElement('article');
+    const cardImg = document.createElement('img');
+    const cardTitle = document.createElement('h2');
+    const cardInfo = document.createElement('p');
+    newCard.id = 'search-result-card';
+    if (show.show.image) {
+      cardImg.src = `${show.show.image.medium}`;
+    } else {
+      cardImg.src = 'https://unsplash.com/photos/ZNTPlG050tk';
+    }
+    cardTitle.textContent = `${show.show.name}`;
+    cardInfo.textContent = `${show.show.summary}`
+    newCard.append(cardImg, cardTitle, cardInfo);
+    searchResultsContainer.append(newCard);
   })
+  form.elements.query.value = '';
 }
 
-formBtn.addEventListener('click', findShow);
+form.addEventListener('submit', findShow);
