e955f72631a90bb9044b44fd59d54639862ab53f
Will Aronow <waronow123@gmail.com>
2021-02-16 21:25:04 -0700

    added extra instructions after finish video to 3 min mark

--- javascript_npm-into/LangGuessr/my-attempt/index.js
+++ javascript_npm-into/LangGuessr/my-attempt/index.js
@@ -4,3 +4,5 @@
 // and accepts an argument which is a string in some language
 // process that string and get the language code for franc
 // use nodejs-lang to process the code from franc to print out the language submitted
+// add error handling for non matches and bad submissions
+// bonus use colors package to change text color of response output
