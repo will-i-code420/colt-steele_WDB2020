1a68640710e21a36087a3b8c8530e56e54071047
Will Aronow <waronow123@gmail.com>
2021-02-16 21:22:52 -0700

    set up LanGuessr solution in seperate folder and created my own attempt folder with index.js and basic instructions from first 2 min of video

---
+++ javascript_npm-into/LangGuessr/my-attempt/index.js
@@ -0,0 +1,6 @@
+// create an app that uses the following npm packages
+// 1) franc - https://github.com/wooorm/franc
+// 2) nodejs-langs - https://github.com/adlawson/nodejs-langs
+// and accepts an argument which is a string in some language
+// process that string and get the language code for franc
+// use nodejs-lang to process the code from franc to print out the language submitted
