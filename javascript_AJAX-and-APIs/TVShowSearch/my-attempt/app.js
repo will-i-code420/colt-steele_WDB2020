b4d7e2c995e04a948f738a2bd313b5fcc6622742
Will Aronow <waronow123@gmail.com>
2021-02-18 19:58:50 -0700

    worked on card more

--- javascript_AJAX-and-APIs/TVShowSearch/my-attempt/app.js
+++ javascript_AJAX-and-APIs/TVShowSearch/my-attempt/app.js
@@ -15,19 +15,30 @@ const findShow = async (e) => {
   shows.forEach((show, i) => {
     const newCard = document.createElement('article');
     const cardImg = document.createElement('img');
-    const cardTitle = document.createElement('h2');
+    const cardBody = document.createElement('div');
+    const cardTitle = document.createElement('h5');
     const cardInfo = document.createElement('p');
-    newCard.classList.add('col', 'col-md-4', 'col-lg-3', 'my-3');
+    const cardBtn = document.createElement('a');
+    newCard.classList.add('card','text-center', 'col', 'col-md-4', 'col-lg-3', 'my-3');
+    cardBody.classList.add('card-body');
+    cardImg.classList.add('card-img-top');
+    cardTitle.classList.add('card-title');
+    cardInfo.classList.add('card-text');
+    cardBtn.classList.add('btn', 'btn-primary');
     if (show.show.image) {
       cardImg.src = `${show.show.image.medium}`;
     } else {
       cardImg.src = 'https://unsplash.com/photos/ZNTPlG050tk';
     }
+    cardImg.alt = `photo of ${show.show.name} characters`;
     const regex = /<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/gi
     const summaryText = show.show.summary;
     cardTitle.textContent = `${show.show.name}`;
     cardInfo.textContent = `${summaryText.replace(regex, '')}`;
-    newCard.append(cardImg, cardTitle, cardInfo);
+    cardBtn.href = `${show.show.officialSite}`;
+    cardBtn.textContent = 'Learn More';
+    cardBody.append(cardTitle, cardInfo, cardBtn);
+    newCard.append(cardImg, cardBody);
     searchResultsContainer.append(newCard);
   })
   form.elements.query.value = '';
