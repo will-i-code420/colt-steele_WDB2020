da36be60ced003997f4ff50e7a9bf3cd1e9409e7
Will Aronow <waronow123@gmail.com>
2021-02-22 22:00:58 -0700

    copied seed file from solution for data to use

--- Mongoose_With_Express_CODE/my-attempt-following/index.js
+++ Mongoose_With_Express_CODE/my-attempt-following/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 
+const Product = require('./models/product');
+
 mongoose.connect('mongodb://localhost:27017/farmAppExample', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("CONNECTED TO MONGO DB")
