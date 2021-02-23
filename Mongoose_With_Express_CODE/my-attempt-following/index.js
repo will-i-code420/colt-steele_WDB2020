e8fa5afcd078bf248fc176ec970d52cf524d678f
Will Aronow <waronow123@gmail.com>
2021-02-22 21:49:56 -0700

    installed mongoose, required mongoose and ejs, set basic connection to mongo db

--- Mongoose_With_Express_CODE/my-attempt-following/index.js
+++ Mongoose_With_Express_CODE/my-attempt-following/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONNECTED TO MONGO DB")
+    })
+    .catch(err => {
+        console.log("ERROR CONNECTING TO MONGO DB")
+        console.log(err)
+    })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
