7c1b287d576c6fcc9fec99e487b1cf8baaa376b7
Will Aronow <waronow123@gmail.com>
2021-02-22 21:52:29 -0700

    a

--- Mongoose_With_Express_CODE/my-attempt-following/index.js
+++ Mongoose_With_Express_CODE/my-attempt-following/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/farmAppExample', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("CONNECTED TO MONGO DB")
     })
