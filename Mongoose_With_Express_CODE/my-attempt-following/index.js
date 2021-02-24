2e3223e24e8e3c6cafdde3fef95eb4d1a2122307
Will Aronow <waronow123@gmail.com>
2021-02-23 20:52:31 -0700

    added method-override, created edit form template and route logic, added links to edit page

--- Mongoose_With_Express_CODE/my-attempt-following/index.js
+++ Mongoose_With_Express_CODE/my-attempt-following/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const ejs = require('ejs');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
 const Product = require('./models/product');
@@ -18,7 +19,8 @@ mongoose.connect('mongodb://localhost:27017/farmAppExample', { useNewUrlParser: 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.get('/products', async (req, res) => {
     const allProducts = await Product.find({});
@@ -35,11 +37,29 @@ app.get('/products/:id', async (req, res) => {
     res.render('products/details', {product});
 });
 
+app.get('/products/:id/edit', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', {product});
+});
+
 app.post('/products', async (req, res) => {
     const {name, price, category} = req.body;
     const newProduct = new Product({name, price, category});
     await newProduct.save();
-    res.redirect(`/products/${newProduct._id}`, {newProduct});
+    res.redirect(`/products/${newProduct._id}`);
+});
+
+app.put('/products/:id', async (req, res) => {
+    const {id} = req.params;
+    const {name, price, category} = req.body;
+    const update = {
+        name,
+        price,
+        category
+    };
+    const product = await Product.findByIdAndUpdate(id, {update}, {new: true, runValidators: true});
+    res.redirect(`/products/${product._id}`);
 });
 
 app.listen(3000, () => {
