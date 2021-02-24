7de85a12822f46c815090f8f977cb0718dbbeef5
Will Aronow <waronow123@gmail.com>
2021-02-23 22:06:35 -0700

    created delete button and route logic

--- Mongoose_With_Express_CODE/my-attempt-following/index.js
+++ Mongoose_With_Express_CODE/my-attempt-following/index.js
@@ -68,6 +68,12 @@ app.put('/products/:id', async (req, res) => {
     res.redirect(`/products/${product._id}`);
 });
 
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
+
 app.listen(3000, () => {
     console.log (`Server Running on Port 3000`);
 });
