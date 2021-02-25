60d321d42399df6e9994b8afe5102486ee13b5f0
Will Aronow <waronow123@gmail.com>
2021-02-24 22:14:36 -0700

    created views folder and home template, installed needed npm packages and setup bare bones express server

---
+++ YelpCamp/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelpCamp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONNECTED TO MONGO DB")
+    })
+    .catch(err => {
+        console.log("ERROR CONNECTING TO MONGO DB")
+        console.log(err)
+    })
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.send('welcome');
+});
+
+app.listen(3000, () => {
+    console.log (`Server Running on Port 3000`);
+});
