9a55ad67f56241c3d4154614f74fd4c689d10c0b
Will Aronow <waronow123@gmail.com>
2021-02-25 21:31:10 -0700

    refactored db connection code

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -4,15 +4,14 @@ const path = require('path');
 const ejs = require('ejs');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const db = mongoose.connection;
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("CONNECTED TO MONGO DB")
-    })
-    .catch(err => {
-        console.log("ERROR CONNECTING TO MONGO DB")
-        console.log(err)
-    })
+
+db.on('error', console.error.bind(console, "db connection error"));
+db.once('open', () => {
+    console.log('DB Connection Successful');
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
