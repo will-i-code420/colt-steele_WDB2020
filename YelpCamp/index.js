0d88bc18640256876c9324e72888d318152275a7
Will Aronow <waronow123@gmail.com>
2021-03-11 20:43:36 -0700

    created delete route and logic for deleting reviews

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -79,6 +79,14 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
+
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
 });
