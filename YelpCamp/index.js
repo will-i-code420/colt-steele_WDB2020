432971241a7a6092b64b231e51cd62746d5ad7f3
Will Aronow <waronow123@gmail.com>
2021-03-14 22:54:03 -0700

    added public folder and added js file for form validation

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/campgrounds', campgroundRoutes)
 app.use('/campgrounds/:id/reviews', reviewRoutes);
