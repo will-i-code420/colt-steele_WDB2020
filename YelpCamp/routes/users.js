a21c79a91fb93ef6b82ffc21cb5fdb3a32c567f7
Will Aronow <waronow123@gmail.com>
2021-03-17 21:09:40 -0700

    created basic login route and template

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router;
 const User = require('../models/user');
 const catchAsync = require('../utilities/catchAsync');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
+});
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
 });
 
 router.post('/register', catchAsync(async (req, res) => {
@@ -19,5 +24,10 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('/register');
     }
 }));
+
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', 'Logged In');
+    res.redirect('/campgrounds');
+});
 
 module.exports = router;
