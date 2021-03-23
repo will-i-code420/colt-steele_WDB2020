a8a008816a8fb1afc2e9c83c4f2b804306591b7e
Will Aronow <waronow123@gmail.com>
2021-03-22 21:22:35 -0700

    moved location of file upload and added to do notes

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -9,12 +9,15 @@ const {storage} = require('../utilities/cloudinary');
 const upload = multer({storage});
 
 // fix post route so validateCampground runs before multer
+// add check for image size and number of images limits
+// make use of cloudinary to resize images to default size
 router.route('/')
     .get(catchAsync(campgroundController.index))
     .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgroundController.createCampground));
 
 router.get('/new', isLoggedIn, campgroundController.renderNewForm);
 // fix put route so validateCampground runs before multer
+// add check for image size and number of images limits
 router.route('/:id')
     .get(catchAsync(campgroundController.getDetails))
     .put(isLoggedIn, isCampgroundAuthor, upload.array('image'), validateCampground, catchAsync(campgroundController.updateCampground))
