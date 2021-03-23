c7c8ff9e56733d42450c27235a1937e822d92a3f
Will Aronow <waronow123@gmail.com>
2021-03-22 22:33:46 -0700

    added deleting images from mongo and cloudinary when deleting

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -2,18 +2,22 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+// create virtual for all campgrounds and campground details
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_250,h_250,c_fill');
+});
+
 const CampgroundSchema = new Schema({
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     reviews: [
         {
@@ -24,7 +28,7 @@ const CampgroundSchema = new Schema({
     description: String,
     location: String
 });
-
+// add delete functions for images both in mongo and cloudinary
 CampgroundSchema.post('findOneAndDelete', async function(doc) {
     if (doc) {
         await Review.deleteMany({
