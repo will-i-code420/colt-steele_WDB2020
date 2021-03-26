220bb7dd1a03686788f9e627b281feb3a841be46
Will Aronow <waronow123@gmail.com>
2021-03-25 22:06:10 -0700

    made seeds file add geometry, tweaked details layout

--- YelpCamp/public/js/detailsMap.js
+++ YelpCamp/public/js/detailsMap.js
@@ -4,7 +4,7 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/light-v10', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10 // starting zoom
+    zoom: 12 // starting zoom
 });
 
 const popup = new mapboxgl.Popup({
--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -3,6 +3,11 @@ const db = mongoose.connection;
 const cities = require('./cities');
 const {descriptors, places} = require('./seedHelpers');
 const Campground = require('../models/campground');
+require('dotenv').config();
+
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const accessToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({accessToken});
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -13,21 +18,27 @@ db.once('open', () => {
 
 const random = arr => arr[Math.floor(Math.random() * arr.length)];
 
-const seedDB = async() => {
+const getGeoData = async location => {
+    const geoData = await geocoder.forwardGeocode({
+        query: location,
+        limit: 1
+    }).send();
+    return geoData.body.features[0].geometry
+}
+
+const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const randomNum = Math.floor(Math.random() * cities.length);
         const price = Math.floor(Math.random() * 40) + 10;
+        const location = `${cities[randomNum].city}, ${cities[randomNum].state}`;
+        const coordinates = await getGeoData(location);
         const campground = new Campground({
             author: '605564810ca2af202de4d0d1',
-            location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
+            location,
             title: `${random(descriptors)} ${random(places)}`,
             description: 'blah blah blah',
             price,
-            geometry: {
-                type: 'Point',
-                coordinates:[-113.1331, 47.0202]
-            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/willicode/image/upload/v1616531906/YelpCamp/qm5ajtqkd94atey7cvgi.jpg',
@@ -39,6 +50,7 @@ const seedDB = async() => {
                 }
             ]
         });
+        campground.geometry = coordinates;
         await campground.save();
     }
 }
