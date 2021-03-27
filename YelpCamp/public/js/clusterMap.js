438311a94bf3b492d10b6886b8d2f0771256854d
Will Aronow <waronow123@gmail.com>
2021-03-26 21:43:09 -0700

    added nav controls to maps

--- YelpCamp/public/js/clusterMap.js
+++ YelpCamp/public/js/clusterMap.js
@@ -6,6 +6,8 @@ style: 'mapbox://styles/mapbox/light-v10',
 center: [-103.59179687498357, 40.66995747013945],
 zoom: 3
 });
+
+map.addControl(new mapboxgl.NavigationControl());
  
 map.on('load', function () {
 // Add a new source from our GeoJSON data and
--- YelpCamp/public/js/detailsMap.js
+++ YelpCamp/public/js/detailsMap.js
@@ -7,6 +7,8 @@ const map = new mapboxgl.Map({
     zoom: 12 // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 const popup = new mapboxgl.Popup({
     offset: 25
 }).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`)
